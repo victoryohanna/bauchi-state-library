8ffbd8cc4c0495821e75b23c496649478b70573a
Victor Yohanna <you@example.com>
2025-11-29 06:41:36 +0100

    add library tpes

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,6 +1,6 @@
 // components/BookCard.tsx
 import Image from "next/image";
-import { Book } from "../../types/library"; // Adjust path to your types file
+import { Book } from "../../../types/library"; 
 
 interface BookCardProps {
   book: Book;
