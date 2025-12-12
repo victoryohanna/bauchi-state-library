c48ea685ee0f79a03fb77ea41a1625e213f286fc
Victor Yohanna <you@example.com>
2025-12-12 07:45:26 +0100

    fix boolgrid id error

--- src/components/BookGrid.tsx
+++ src/components/BookGrid.tsx
@@ -1,8 +1,5 @@
-
-
-
 import { BookCard } from "./BookCard";
-import { Book } from "../types/library"; 
+import { Book } from "../types/library";
 
 interface BookGridProps {
   books: Book[];
@@ -38,7 +35,7 @@ export const BookGrid = ({ books }: BookGridProps) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {books.map((book) => (
-        <BookCard key={book.id} book={book} />
+        <BookCard key={book._id} book={book} />
       ))}
     </div>
   );
